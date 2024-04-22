3ec8639f29e703c61a62bc088733ccc8b9474591
aritra995 <aritrasiliguri@gmail.com>
2024-04-22 12:11:39 +0530

    implemented createUser POST method reading request body

--- src/main/java/com/thoughtworks/app/ws/ui/controller/UserController.java
+++ src/main/java/com/thoughtworks/app/ws/ui/controller/UserController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thoughtworks.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.thoughtworks.app.ws.ui.model.response.UserRest;
 
 import org.springframework.http.HttpStatus;
@@ -40,9 +43,22 @@ public class UserController {
 		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK );
 	}
 	
-	@PostMapping
-	public String createUser() {
-		return "create user was called";
+	@PostMapping( 
+			consumes = {
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE
+					}, 
+			produces = {
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE
+					} )
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		returnValue.setEmail(userDetails.getEmail());
+		
+		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK );
 	}
 	
 	@PutMapping
