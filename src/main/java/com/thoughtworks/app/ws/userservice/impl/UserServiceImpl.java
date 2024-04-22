6e6467f9fa6ddb49787002e96c7b0fca14a5ec8d
aritra995 <aritrasiliguri@gmail.com>
2024-04-22 17:27:24 +0530

    added constructor based dependency injection in UserServiceImpl

--- src/main/java/com/thoughtworks/app/ws/userservice/impl/UserServiceImpl.java
+++ src/main/java/com/thoughtworks/app/ws/userservice/impl/UserServiceImpl.java
@@ -1,17 +1,28 @@
 package com.thoughtworks.app.ws.userservice.impl;
 
+import com.thoughtworks.app.ws.shared.Utils;
 import com.thoughtworks.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.thoughtworks.app.ws.ui.model.response.UserRest;
 import com.thoughtworks.app.ws.userservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
     Map<String, UserRest> users;
+
+    Utils utils;
+
+    public UserServiceImpl() {
+    }
+
+    @Autowired
+    public UserServiceImpl(Utils utils) {
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -20,7 +31,7 @@ public class UserServiceImpl implements UserService {
         returnValue.setLastName(userDetails.getLastName());
         returnValue.setEmail(userDetails.getEmail());
 
-        String userId = UUID.randomUUID().toString();
+        String userId = utils.generateUserId();
         returnValue.setUserId(userId);
 
         if (users == null) users = new HashMap<>();
